d153ed83ca45514329f2c02dfa721fcb0d6be674
vamit5 <vamit5.team@gmail.com>
2026-09-25 21:53:34 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+// EXATHLEAGUE: omogućava instalaciju na telefon. Podaci se uvek čitaju uživo.
+const C='exathleague-v1';
+const SHELL=['./','index.html','logo.jpg','mark.png','wordmark.png','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=='GET'||u.origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request)));
+});
